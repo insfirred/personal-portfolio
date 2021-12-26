c896348329f0783a8a9565ad0cba9c7255996c6b
Kalash <kalash27k@gmail.com>
2021-12-26 15:59:17 +0530

    8th commit, updated navigation bar in mobile view

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('popbar').addEventListener('click', function() {
+    document.getElementById('navlinks').style.left = "0";
+})
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('navlinks').style.left = "43vw";
+})
+document.getElementById('edu-link').addEventListener('click', function() {
+    document.getElementById('navlinks').style.left = "43vw";
+})
+document.getElementById('project-link').addEventListener('click', function() {
+    document.getElementById('navlinks').style.left = "43vw";
+})
+document.getElementById('contact-link').addEventListener('click', function() {
+    document.getElementById('navlinks').style.left = "43vw";
+})
